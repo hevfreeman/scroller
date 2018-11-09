b0ac8e954a3fb119d28a813259d5597f51f71b7d
Morev Ilya <ilya.morev@danfoss.com>
2018-11-09 19:34:23 +0300

    cool

--- main.py
+++ main.py
@@ -6,6 +6,41 @@ SCREEN_HEIGHT = 600
 SPRITE_SCALING_PLAYER = 0.4
 SPRITE_SCALING_ENEMY = 0.4
 
+MOVEMENT_SPEED = 3
+ENEMY_MOVEMENT_SPEED = 2
+
+
+class Player(arcade.Sprite):
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+
+
+class Enemy(arcade.Sprite):
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+
 class MyGame(arcade.Window):
     """ Main application class. """
 
@@ -16,8 +51,10 @@ class MyGame(arcade.Window):
 
         # self.player_list = arcade.SpriteList()
         self.enemy_list = arcade.SpriteList()
-        self.player_sprite = arcade.Sprite("img/player.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite = Player("img/player.png", SPRITE_SCALING_PLAYER)
         self.enemy_sprite = arcade.Sprite("img/enemy.png", SPRITE_SCALING_ENEMY)
+
+        self.enemy_list.append(self.enemy_sprite)
 
         self.player_sprite.center_x = 150
         self.player_sprite.center_y = 50
@@ -39,7 +76,32 @@ class MyGame(arcade.Window):
 
     def update(self, delta_time):
         """ All the logic to move, and the game logic goes here. """
+        self.player_sprite.update()
+        for enemy in self.enemy_list:
+            self.enemy_sprite.change_x = -ENEMY_MOVEMENT_SPEED
+            self.enemy_sprite.change_y = 0
+            enemy.update()
         pass
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
 
 
 def main():
